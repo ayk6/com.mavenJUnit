18a8249beabd2e435514cbd62734cfa98939510c
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-06-10 18:39:33 +0300

    day11rev

---
+++ src/test/java/day11/C03actions.java
@@ -0,0 +1,11 @@
+package day11;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C03actions extends TestBase {
+    @Test
+    public void test1(){
+        driver.get("https://www.amazon.com");
+    }
+}
